d7db429d2cba052f77eeccd06440fd3277e6206b
Samir70 <sbetmouni@talktalk.net>
2021-09-12 10:26:37 +0100

    show topics when course selected

--- public/Components/courseCard.js
+++ public/Components/courseCard.js
@@ -1,9 +1,9 @@
 Vue.component('course-card', {
     props: {
-        courseData: Object
+        courseData: Object,
     },
     template: `
-        <div class="course-card">
+        <div class="course-card" v-on:click="$emit('course-chosen', courseData)">
         <h1>{{courseData.courseName}}</h1>
         <p>Completed 0 / {{courseData.topics.length}} topics</p>
         <p>Your rating: 1234</p>
